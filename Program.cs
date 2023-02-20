bb90e352535fa40be8a5ec3db15960ab66e69134
Divu <d1403kr3@gmail.com>
2023-02-20 18:12:38 +0530

    Assignment 6

--- Program.cs
+++ Program.cs
@@ -8,19 +8,27 @@ namespace Assignment_6
     {
         public static async Task Main(string[] args)
         {
-            // Asking user to input the URL
-            Console.Write("Enter the URL: ");
-            var url = Console.ReadLine();
+            try
+            {
+                // Asking user to input the URL
+                Console.Write("Enter the URL: ");
+                var url = Console.ReadLine();
 
-            // Here I am using HttpClient() instance
-            using var client = new HttpClient();
-            var content = await client.GetStringAsync(url);
+                // Here I am using HttpClient() instance
+                using var client = new HttpClient();
+                var content = await client.GetStringAsync(url);
 
-            // Writing the content in A.txt file
-            await File.WriteAllTextAsync("A.txt", content);
+                // Writing the content in A.txt file
+                await File.WriteAllTextAsync("A.txt", content);
 
-            // Printing the success message
-            Console.WriteLine("Successfully written to A.txt file");
+                // Printing the success message
+                Console.WriteLine("Successfully written to A.txt file");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid URL");
+            }
 
         }
     }
